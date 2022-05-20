6e9f99831615212144939cfda9159873bc50ecf9
Full stack monster <91737212+profullstackdeveloper@users.noreply.github.com>
2022-05-20 07:39:30 -0700

    add test function

--- server/controllers/calculator.controller.js
+++ server/controllers/calculator.controller.js
@@ -21,3 +21,9 @@ module.exports.calculator = (req, res) => {
     stdev: stdev(numbers),
   });
 };
+
+module.exports.test = (req, res) => {
+  res.status(200).json({
+    message: "this is test text"
+  });
+}
